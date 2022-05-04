77659088236e51713967ee12764d198dfabd1e5a
kachun <kcwong8051@gmail.com>
2022-05-04 21:45:30 +0800

    less than zero would not be decreased

--- src/Counter.elm
+++ src/Counter.elm
@@ -3,10 +3,12 @@ import Html exposing (..)
 import Html.Events exposing(..)
 import Browser
 
+-- model is int
 type alias Model = Int
 myModel: Model
 myModel = 0
 
+-- message type
 type Msg = IncrementMsg | DecrementMsg
 
 myView: Model -> Html Msg
@@ -17,11 +19,12 @@ myView model =
        ,button [onClick IncrementMsg] [text "+"]
     ]
 
+-- input which message & old data -> new data
 myUpdate: Msg -> Model -> Model
 myUpdate msg model = 
    case msg of
        IncrementMsg -> model + 1
-       DecrementMsg -> model - 1
+       DecrementMsg -> if model > 0 then model - 1 else model
 
 main: Program() Model Msg
 main = 
